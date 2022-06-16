e59318b41863c2eb3da0429e5805e5c6aae00213
Michael Kriese <michael.kriese@visualon.de>
2022-06-16 11:56:47 +0200

    chore: simplify

---
+++ test.tf
@@ -0,0 +1,42 @@
+
+
+resource "kubernetes_deployment" "octant" {
+  count = 1
+  metadata {
+    name      = "octant"
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = { app = "octant" }
+    }
+    template {
+      spec {
+       
+        init_container {
+          name    = "init-sh"
+          image   = "amazon/aws-cli:2.6.4"
+        }
+
+        container {
+          name  = "node"
+          image = "python:3.6"
+        }
+
+        container {
+          name  = "octant"
+          image = "amazon/aws-cli:2.6.4"
+         
+        }
+
+        init_container {
+          name  = "node"
+          image = "node:14"
+        }
+       
+      }
+    }
+   
+  }
+}
+
